133b72df964b1f99144e78877238baddd2a49d88
Abdullah Al Hadi <abdullahalhadi325@gmail.com>
2026-07-07 20:10:12 +0600

    Update vite base path to my-portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio11/",
+  base: "/my-portfolio/",
   plugins: [react()]
 })
+
